4a3ebc27352a54105e944d6254b5fb6d79b0ff74
OneRite <steamplay2017@gmail.com>
2024-03-18 11:02:07 +0300

    fix:

--- laba_2/main.cpp
+++ laba_2/main.cpp
@@ -1,30 +1,29 @@
 ﻿
 //Задача №2
 //“Первый класс”
-//
-//Создайте класс, описывающий указанный в вашем варианте реальный объект.Не забывайте про принципы абстракции и инкапсуляции.
+//Создайте класс, описывающий указанный в вашем варианте реальный объект.
+//Не забывайте про принципы абстракции и инкапсуляции.
 //Продемонстрируйте использование вашего класса.
 //Треугольник, расчет площади, периметра.
 #include <iostream>
 #include <cmath>
 
-class Triangle{
+class Triangle 
+{
     float side1;
     float side2;
     float side3;
 public:
-    // Конструктор по умолчанию
-    Triangle() = default;
-    Triangle(float a, float b, float c) {
-        side1 = a;
-        side2 = b;
-        side3 = c;
-    }
-   
-    float calculateP() {
+    // Конструктор по умолчанию с параметрами по умолчанию
+    Triangle(float a = 1, float b = 1, float c = 1) : side1(a), side2(b), side3(c) {}
+
+    float calculateP() 
+    {
         return side1 + side2 + side3;
     }
-    float calculateS() {
+
+    float calculateS() 
+    {
         // Полупериметр
         float s = calculateP() / 2;
         // Площадь по формуле Герона
@@ -35,16 +34,17 @@ public:
 int main() 
 {
     setlocale(LC_ALL, "Russian");
-  // Создаем объект треугольника
-    Triangle t(-4, 3, 6);
+    // Создаем объект треугольника с конструктором по умолчанию
+    Triangle t;
 
-  // Рассчитывам P и S треугольника
+    // Рассчитываем P и S треугольника
     float P = t.calculateP();
     float S = t.calculateS();
 
-  // Вывод результата
+    // Вывод результата
     std::cout << "Периметр треугольника: " << P << std::endl;
     std::cout << "Площадь треугольника: " << S << std::endl;
 
     return 0;
 }
+
